211b17b73886235218060e6718ba37e0183c536e
NaEun Kim <121096778+hellonaeunkim@users.noreply.github.com>
2025-09-05 12:26:37 +0900

    [level 1] Title: [1차] 비밀지도, Time: 2.74 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -8,9 +8,6 @@ class Solution {
         // answer담을 배열 선언
         String[] answer = new String[n];
         
-        // 2진수를 해독한 문자 저장하기 위해 StringBuilder 선언
-        StringBuilder sb = new StringBuilder();
-        
         // arr1, arr2 각 배열 값 2진수로 변환
         for(int i = 0; i < arr1.length; i++) {
             newArr1[i] = toBinary(arr1[i], n);
@@ -18,7 +15,9 @@ class Solution {
         }
         
         // arr1, arr2 값 비교해서 해독
-        for (int i = 0; i < n; i++) {            
+        for (int i = 0; i < n; i++) {    
+            // 2진수를 해독한 문자 저장하기 위해 StringBuilder 선언
+            StringBuilder sb = new StringBuilder();
             for (int j = 0; j < n; j++) {
                 char c1 = newArr1[i].charAt(j);
                 char c2 = newArr2[i].charAt(j);
@@ -33,28 +32,19 @@ class Solution {
             
             // 완성된 한 줄을 answer에 저장
             answer[i] = sb.toString();
-            sb = sb.delete(0, n);
         }
         return answer;
     }
     
     // 2진수 변환 메서드
     private static String toBinary(int num, int n) {
+        // 2진수 변환
+        String st = Integer.toString(num, 2);
         
-        StringBuilder sb = new StringBuilder();
-        
-        while (num > 0) {
-            int remainder = num % 2; // 나머지 구하기
-            sb.append(remainder); // int형 자동으로 String형으로 저장
-            num /= 2; // 몫으로 갱신
-        }
-        
-        String st = sb.reverse().toString(); // 뒤집어서 반환
-        
+        // 길이가 n보다 짧으면 앞에 '0'을 붙여서 n자리 맞추기
         while (st.length() < n) {
             st = "0" + st;
         }
-        System.out.println(st);
         return st;
     }
 }
